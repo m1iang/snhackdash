fc9d0414fa940f31bdd33b824a1390d291a194b9
Simon Shen <cubixularhelix@gmail.com>
2023-02-11 03:15:57 -0500

    base files

---
+++ src/router/routes.ts
@@ -0,0 +1,23 @@
+import Home from '@/views/Home.vue';
+import NotFound from '@/views/NotFound.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound,
+  }
+]
+
+export default routes;
+
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+export default {
+  title: "Home",
+};
+</script>
+<template>
+  <div class="home">
+	  <h1>Hello</h1>
+  </div>
+</template>
+<style scoped>
+</style>
