f1ceabe5e0b57a2b84bd04aaa2aa00d0874ce98a
Mike Liang <liang.mike.to@gmail.com>
2023-02-11 03:07:52 -0500

    get server configs set up

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import '@/style.css'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+import { VueFire, VueFireAuth } from "vuefire"
+import { app, auth } from "@/config/firebaseConfig"
+
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
+
+
+createApp(App)
+  .use(VueFire, {
+    firebaseApp: app,
+    modules: [
+      VueFireAuth(),
+    ],
+  })
+  .component('v-icon', OhVueIcon)
+  .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "./src"),
+      '@views': path.resolve(__dirname, "./src/views"),
+      '@assets': path.resolve(__dirname, "./src/assets"),
+      '@components': path.resolve(__dirname, "./src/components"),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  }
 })
